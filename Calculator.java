13efa59bbf772550c0af4110ffb0fb0d694816fb
Eduardo Cabreja <eduardojcabreja@gmail.com>
2024-09-26 20:29:02 +0200

    Caracteristica: Implementa la multiplicacion

--- Calculator.java
+++ Calculator.java
@@ -13,15 +13,26 @@ public class Calculator {
 
         double result = num1 + num2;
         System.out.println("El resultado de la suma es: " + result);
-	// Añadir después de la implementación de la suma
-   System.out.println("Ingrese el primer número para la resta:");
-   double num3 = scanner.nextDouble();
+        // Añadir después de la implementación de la suma
+       System.out.println("Ingrese el primer número para la resta:");
+       double num3 = scanner.nextDouble();
 
-   System.out.println("Ingrese el segundo número para la resta:");
-   double num4 = scanner.nextDouble();
+       System.out.println("Ingrese el segundo número para la resta:");
+       double num4 = scanner.nextDouble();
 
-   double resultSub = num3 - num4;
-   System.out.println("El resultado de la resta es: " + resultSub);
+       double resultSub = num3 - num4;
+       System.out.println("El resultado de la resta es: " + resultSub);
+
+        // Añadir después de la implementación de la multiplicacion
+        System.out.println("Ingrese el primer número para la multiplicación:");
+        double num5 = scanner.nextDouble();
+
+        System.out.println("Ingrese el segundo número para la multiplicación:");
+        double num6 = scanner.nextDouble();
+
+        double resultMul = num5 * num6;
+        System.out.println("El resultado de la multiplicación es: " + resultMul);
+
 
         scanner.close();
     }
